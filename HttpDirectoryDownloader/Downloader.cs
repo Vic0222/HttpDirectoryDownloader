dc70e9d9c03f660bd55aefaa0bb300ad8c9f5744
Vic0111 <v.g.albonian@gmail.com>
2018-06-24 19:03:56 +0800

    Add project files.

---
+++ HttpDirectoryDownloader/Downloader.cs
@@ -0,0 +1,67 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpDirectoryDownloader
+{
+    public class Downloader: IDisposable
+    {
+        
+        public Downloader()
+        {
+        }
+
+        public async Task DownloadDirectory(string httpDirectory, string downloadPath)
+        {
+            using (var httpClient = new HttpClient())
+            {
+
+                var response = await httpClient.GetAsync(httpDirectory);
+                string body = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    
+                    throw new Exception(string.Format("Http Error : {0} \n Message Body: {1}",response.StatusCode.ToString(), body));
+                }
+                var htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(body);
+                var anchors = htmlDoc.DocumentNode.SelectNodes("//a");
+                ICollection<FileInfo> fileInfoes = anchors.Select(a => new FileInfo() { FileName = a.InnerText, Link = a.GetAttributeValue("href", null) }).ToList();
+                foreach (var fileInfo in fileInfoes)
+                {
+                    var uri = new Uri(new Uri(httpDirectory), fileInfo.Link);
+                    var getResponse = await httpClient.GetAsync(uri);
+                    if (!getResponse.IsSuccessStatusCode && getResponse.StatusCode != HttpStatusCode.NotFound)
+                    {
+                        string message = await getResponse.Content.ReadAsStringAsync();
+                        throw new Exception(string.Format("Http Error : {0} \n Message Body: {1}", getResponse.StatusCode.ToString(), body));
+                    }
+
+                    Stream file = await getResponse.Content.ReadAsStreamAsync();
+                    using (file)
+                    {
+                        var path = Path.Combine(downloadPath, fileInfo.FileName);
+                        using (var streamWriter = new FileStream(path, FileMode.Create))
+                        {
+                            file.Seek(0, SeekOrigin.Begin);
+                            file.CopyTo(streamWriter);
+                        }
+                    }
+                }
+               
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Test.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var downloader = new HttpDirectoryDownloader.Downloader();
+            downloader.DownloadDirectory(@"http://localhost/TestDirectory/", @"C:\temp").Wait();
+        }
+    }
+}
